21e05173b5d8886e2e1f138bb916a07739437744
Herschdorfer <herschdorfer@gmail.com>
2024-01-30 22:31:30 +0100

    use tasks

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "ESP32_PWM.h"
 #include "HardwareSerial.h"
 #include "WiFi.h"
+#include "esp32-hal.h"
 #include "esp_task_wdt.h"    // Include the header file for ESP watchdog
 #include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <ArduinoJson.h>
@@ -23,6 +24,11 @@
 
 #define HW_TIMER_INTERVAL_US 20L
 
+const char *const mqtt_server = "omv"; // mqtt server
+
+const char *const topic_status = "/boiler/status";
+const char *const topic_power_max = "/boiler/control";
+
 WiFiClient espClient;
 PubSubClient client(espClient); // lib required for mqtt
 
@@ -40,13 +46,9 @@ ESP32_PWM ISR_PWM;
 ESP32Timer ITimer(1);
 uint32_t PWM_Pin = TFT_BL; // Pin 2 = ESP32 GPIO 2
 
-const char *const mqtt_server = "omv"; // mqtt server
-
-const char *const topic_status = "/boiler/status";
-const char *const topic_power_max = "/boiler/control";
-
-int power_max = 0;
-int power_desired = 0;
+uint16_t power_max = 0;
+uint16_t power_desired = 0;
+uint16_t power_actual = 0;
 
 uint8_t channelNum;
 
@@ -54,8 +56,6 @@ float PWM_Freq1 = 100.0f;
 
 // You can assign any duty_cycle for any PWM here, from 0-100
 float PWM_DutyCycle1 = 50.0;
-
-volatile uint32_t startMicros = 0;
 
 void parse_json(byte *payload, unsigned int length) {
 
@@ -69,9 +69,13 @@ void parse_json(byte *payload, unsigned int length) {
   if (doc.containsKey("power_desired")) {
     power_desired = doc["power_desired"];
   }
+
+  if (doc.containsKey("power_actual")) {
+    power_actual = doc["power_actual"];
+  }
 }
 
-void callback(char *topic, byte *payload, unsigned int length) {
+void callback(char const *topic, byte *payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
@@ -111,10 +115,57 @@ void connectMqtt() {
   }
 }
 
-bool IRAM_ATTR TimerHandler(void *timerNo) {
+bool IRAM_ATTR TimerHandler(void *) {
   ISR_PWM.run();
 
   return true;
+}
+
+[[noreturn]] void handleTft(void *) {
+  for (;;) {
+    tft.setTextWrap(false);
+    tft.setCursor(0, 0);
+    tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
+    tft.setTextSize(1);
+
+    tft.println("Connected to: ");
+    tft.println(wifiManager.getWiFiSSID());
+    tft.println("IP address: ");
+    tft.println(WiFi.localIP());
+
+    tft.print("Uptime: ");
+    tft.println(uptime.getUptimeInSeconds());
+
+    tft.print("Power max: ");
+    tft.print(power_max);
+    tft.println("    ");
+
+    tft.print("Power des: ");
+    tft.print(power_desired);
+    tft.println("    ");
+
+    tft.print("Power act: ");
+    tft.print(power_actual);
+    tft.println("    ");
+
+    tft.print("Duty Cycle: ");
+    tft.print(PWM_DutyCycle1);
+    tft.println("%   ");
+
+    delay(1000);
+  }
+}
+
+[[noreturn]] void handleMqtt(void *) {
+  for (;;) {
+    if (!client.connected()) {
+      reconnect();
+    }
+
+    client.loop();
+
+    delay(100);
+  }
 }
 
 void setup(void) {
@@ -122,60 +173,35 @@ void setup(void) {
 
   wifiManager.autoConnect("Boiler-Controller");
 
+  client.setServer(mqtt_server, 1883); // connecting to mqtt server
+  client.setCallback(callback);
+
   pinMode(TFT_BL, OUTPUT); // TTGO T-Display enable Backlight pin 4
   // digitalWrite(TFT_BL, HIGH); // T-Display turn on Backlight
   tft.init(135, 240); // Initialize ST7789 240x135
   tft.fillScreen(ST77XX_BLACK);
 
-  client.setServer(mqtt_server, 1883); // connecting to mqtt server
-  client.setCallback(callback);
-
   connectMqtt();
 
-  // Interval in microsecs
   if (ITimer.attachInterruptInterval(HW_TIMER_INTERVAL_US, TimerHandler)) {
-    startMicros = micros();
-    Serial.print(F("Starting ITimer OK, micros() = "));
-    Serial.println(startMicros);
+    Serial.print(F("Starting ITimer OK = "));
   } else
     Serial.println(F("Can't set ITimer. Select another freq. or timer"));
 
-  channelNum = ISR_PWM.setPWM(PWM_Pin, PWM_Freq1, PWM_DutyCycle1);
-}
+  channelNum = (uint8_t)ISR_PWM.setPWM(PWM_Pin, PWM_Freq1, PWM_DutyCycle1);
 
-void handleTft() {
-  tft.setTextWrap(false);
-  tft.setCursor(0, 0);
-  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);
-  tft.setTextSize(1);
+  xTaskCreate(handleTft, "Task_TFT", 2000, nullptr, 1, nullptr);
 
-  tft.println("Connected to: ");
-  tft.println(wifiManager.getWiFiSSID());
-  tft.println("IP address: ");
-  tft.println(WiFi.localIP());
-
-  tft.println("Uptime: ");
-  tft.println(uptime.getUptimeInSeconds()); // Display the uptime counter
-
-  tft.println("Power max: ");
-  tft.println(power_max);
-
-  tft.println("Power desired: ");
-  tft.println(power_desired);
-
-  // Rest of your code...
+  xTaskCreate(handleMqtt, "Task_MQTT", 2000, nullptr, 2, nullptr);
 }
 
 void loop() {
   esp_task_wdt_init(1,
-                    true); // Enable ESP watchdog with a timeout of 10 seconds
-  handleTft();
-  delay(50);
+                    true); // Enable ESP watchdog with a timeout of 1 seconds
 
   if (!client.connected()) {
     reconnect();
   }
-  client.loop();
 
   if (power_desired > 0 && power_max > 0 && power_desired <= power_max) {
     PWM_DutyCycle1 =
@@ -189,7 +215,7 @@ void loop() {
                                 PWM_DutyCycle1)) {
     Serial.print(F("modifyPWMChannel error for PWM_Period"));
   }
-  esp_task_wdt_reset();
+  delay(50);
 
-  // Rest of your loop code...
+  esp_task_wdt_reset();
 }
